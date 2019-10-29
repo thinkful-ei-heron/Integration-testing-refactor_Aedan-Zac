91daf37dab97756d69351c5c9d853248d3f3dc01
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-10-29 09:13:18 -0700

    added sort functionality

--- app.js
+++ app.js
@@ -6,7 +6,47 @@ app.use(morgan('common'));
 const playStore = require('./playstore.js');
 
 app.get('/apps',(req,res) => {
+
+  let results = playStore;
+  let { genre, sort} = req.query;
+  // If genre or sort is provided
+
+  if (genre){
+    genre.toLowerCase();
+  }
+
+  if(sort){
+    sort = sort.toLowerCase();
+    sort = sort.charAt(0).toUpperCase() + sort.slice(1);
+    if(sort === 'Rating'){
+      results.sort((a,b)=>{
+        return a[sort] < b[sort] ? 1 : -1;
+      });
+    }
+    if (sort === 'App'){
+      results.sort((a,b)=>{
+        return a[sort].toUpperCase() > b[sort].toUpperCase() ? 1 : -1;
+      });
+    }
+  }
+  // if (sort){
+  //   results.sort((a,b)=>{
+  //     return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+  //   });
+  // }
+
+  // validate the input
+
+  // logic of sorting
+
   
+  //   .filter(book =>
+  //     book
+  //       .title
+  //       .toLowerCase()
+  //       .includes(search.toLowerCase()));
+
+  return res.json(results);
 });
 
 app.listen(8000, () =>{

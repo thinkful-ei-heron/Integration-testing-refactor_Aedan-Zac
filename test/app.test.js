6413a6f968c5abfac9b0a064e107b7553acc0d47
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-10-30 13:57:09 -0500

    genre tests complete

--- test/app.test.js
+++ test/app.test.js
@@ -74,4 +74,63 @@ describe('App GET /apps pathway', () => {
                 expect(boolSort).to.be.true;
             });
     });
+    it('Responds with 400 and "Must filter by " if genre is not of values Action, Puzzle, Strategy, Casual, Arcade, Card', () => {
+        return supertest(app)
+            .get('/apps')
+            .query({genre: 'something invalid'})
+            .expect(400, 'Must be of genre Action, Puzzle, Strategy, Casual, Arcade, Card');
+    });
+    it('Responds with 200 and search results if provided a valid genre value of Action including random caplitalized letters', () => {
+        return supertest(app)
+            .get('/apps')
+            .query({genre:'acTioN'})
+            .expect(200)
+            .then(res => {
+                expect(res.body).to.be.an('Array');
+                expect(res.body.length).to.equal(6);
+                expect(res.body[0]).to.be.an('Object');
+                expect(res.body[0]).to.have.all.keys('App','Category','Rating','Reviews','Size','Installs','Type','Price','Content Rating','Genres','Last Updated','Current Ver','Android Ver');
+                let boolFilter = true;
+                let i = 0;
+                while(boolFilter === true && i < res.body.length-1) {
+                    if(!res.body[i].Genres.toLowerCase().includes('action')) {
+                        boolFilter = false;
+                    }
+                    i++;
+                }
+                expect(boolFilter).to.be.true;
+            });
+    });
+    it('Responds with 200 and sorted search results if provided a valid sort by App and genre value of Action including random caplitalized letters', () => {
+        return supertest(app)
+            .get('/apps')
+            .query({genre:'aCtIoN', sort:'apP'})
+            .expect(200)
+            .then(res => {
+                expect(res.body).to.be.an('Array');
+                expect(res.body.length).to.equal(6);
+                expect(res.body[0]).to.be.an('Object');
+                expect(res.body[0]).to.have.all.keys('App','Category','Rating','Reviews','Size','Installs','Type','Price','Content Rating','Genres','Last Updated','Current Ver','Android Ver');
+                
+                let boolFilter = true;
+                let i = 0;
+                while(boolFilter === true && i < res.body.length-1) {
+                    if(!res.body[i].Genres.toLowerCase().includes('action')) {
+                        boolFilter = false;
+                    }
+                    i++;
+                }
+                expect(boolFilter).to.be.true;
+
+                let boolSort = true;
+                let j = 0;
+                while(boolSort === true && j < res.body.length-1) {
+                    if(!res.body[j].App < res.body[j+1].App) {
+                        boolSort = false;
+                    }
+                    j++;
+                }
+                expect(boolSort).to.be.true;
+            });
+    });
 });
